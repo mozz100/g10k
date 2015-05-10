8886c112a1c1076412d4dbbb37430d6fc71a3f61
Richard Morrison <richard@rmorrison.net>
2015-05-10 21:17:56 +0100

    Expose race data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
+require 'json'
+
 class WelcomeController < ApplicationController
   def index
+  end
+
+  def race
+  	@race = Race.first
+  	render text: JSON.pretty_generate(@race.as_json), content_type: "text/json"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Race.reset
+Runner.delete_all
+
+race = Race.first
+
+Runner.create name: "Tortoise", email: "tortoise@example.com", expected_duration: 999.9, race_id: race.id
+
+Runner.create name: "Dog", email: "dog@example.com", expected_duration: 100.0, race_id: race.id
+
+Runner.create name: "Hare", email: "hare@example.com", expected_duration: 29.9, race_id: race.id
+
+race.start_time = Time.now + 2.hours
+race.save
