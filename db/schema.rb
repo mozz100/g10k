c55bffc95f77440bfa02df01be33242f4a7294b8
Richard Morrison <richard@rmorrison.net>
2015-05-17 23:46:27 +0100

    Docker-compose stuff

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150317040203) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "check_points", force: :cascade do |t|
     t.integer  "runner_id"
     t.decimal  "percent"
